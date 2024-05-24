8a2cc489f5716907d9e83c3e5d9b0baf7232d4a4
vaporyhumo <roanvilina@gmail.com>
2024-05-24 16:58:16 -0400

    Add `FSON::Fraction`

---
+++ lib/fson/fraction.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+module FSON
+  class Fraction
+    def self.parse(string)
+      if string[0] == "."
+        digits = Digits.parse(string[1..])
+
+        if digits.none?
+          Maybe.return Result.new(None.new, string)
+        else
+          Maybe.return Result.new(Some.new(digits.value!.fetch(0)), digits.value!.fetch(1))
+        end
+      else
+        Maybe.return Result.new(None.new, string)
+      end
+    end
+
+    class None < self
+      def ==(other)
+        self.class == other.class
+      end
+    end
+
+    class Some < self
+      def initialize(digits)
+        @digits = digits
+      end
+
+      attr_reader :digits
+
+      def ==(other)
+        self.class == other.class &&
+          self.digits == other.digits
+      end
+    end
+  end
+end
---
+++ spec/fson/fraction_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require_relative '../../lib/fson'
+
+RSpec.describe FSON::Fraction do
+  describe '.parse' do
+    it 'might be empty' do
+      string = ''
+      result = described_class.parse(string)
+
+      expect(result).to eq(Maybe.return(FSON::Result.new(described_class::None.new, "")))
+    end
+
+    it 'needs digits' do
+      string = "."
+      result = described_class.parse(string)
+
+      expect(result).to eq(Maybe.return(FSON::Result.new(described_class::None.new, string)))
+    end
+
+    it 'parses the fraction' do
+      string = ".1"
+      result = described_class.parse(string)
+      digits = FSON::Digits.parse(string[1..]).value!.fetch(0)
+
+      token = described_class::Some.new(digits)
+
+      expect(result).to eq(Maybe.return(FSON::Result.new(token, "")))
+    end
+  end
+end
