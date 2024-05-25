4d8d58b9d938e0e054cf2150bb885b63ca11da66
vaporyhumo <roanvilina@gmail.com>
2024-05-25 01:20:14 -0400

    Lint `spec/fson/exponent_spec.rb`

--- spec/fson/exponent_spec.rb
+++ spec/fson/exponent_spec.rb
@@ -10,28 +10,36 @@ RSpec.describe FSON::Exponent do
       expect(result).to eq(Maybe.none)
     end
 
-    it 'parses an exponent' do
+    it 'does not parse just an "E" letter' do
       str = %w[E e].sample
       result = described_class.parse(str)
       expect(result).to eq(Maybe.none)
     end
 
-    it 'parses an exponent' do
+    it 'does not parse if there are no digits' do
       str = ['E-', 'E+', 'e-', 'e+'].sample
       result = described_class.parse(str)
       expect(result).to eq(Maybe.none)
     end
 
-    it 'parses an exponent' do
-      e_letter = %w[E e].sample
-      sign = ['+', '-', ''].sample
-      sign_obj = FSON::Sign.parse(sign).value!.token
-      digits = rand(1000).to_s
-      digits_obj = FSON::Digits.parse(digits).value!.token
+    context 'when successful' do
+      let(:sign) { ['+', '-', ''].sample }
+      let(:digits) { rand(1000).to_s }
+      let(:string) do
+        e_letter = %w[E e].sample
+        [e_letter, sign, digits.to_s].join
+      end
+      let(:sign_obj) { FSON::Sign.parse(sign).value!.token }
+      let(:digits_obj) { FSON::Digits.parse(digits).value!.token }
 
-      str = [e_letter, sign, digits.to_s].join
-      result = described_class.parse(str)
-      expect(result).to eq(Maybe.return(FSON::Result.new(described_class.new(sign_obj, digits_obj), '')))
+      it 'parses an exponent' do
+        actual = described_class.parse(string)
+        expected = Maybe.return(
+          FSON::Result.new(described_class.new(sign_obj, digits_obj), '')
+        )
+
+        expect(actual).to eq(expected)
+      end
     end
   end
 end
